03ffa1396c7dd2d0e268704e02409ceb804e38a5
Engin Bolat <engi.bolat@gmail.com>
2026-05-17 22:48:17 +0300

    refactor: simplify movie card styles by removing unused dimensions import
    - Removed the unused `Dimensions` import from the movie card styles file for cleaner code.

--- src/shared/components/movie-card-with-description/style.ts
+++ src/shared/components/movie-card-with-description/style.ts
@@ -1,8 +1,6 @@
-import { Dimensions, StyleSheet } from 'react-native'
+import { StyleSheet } from 'react-native'
 
 import { scaleHeight, scaleWidth } from '@shared/helpers/helper'
-
-const { width } = Dimensions.get('window')
 
 export const styles = StyleSheet.create({
   container: {
